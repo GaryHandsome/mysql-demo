09ea56d1ce5082343ef4f3e0afa2101882d8aea5
zqx <114729040@qq.com>
2023-03-02 10:48:07 +0800

    mysql

--- sql/ch04-性能优化.sql
+++ sql/ch04-性能优化.sql
@@ -98,4 +98,165 @@ show profile for query 93;
 # 语法： show profile cpu for query query_id;
 show profile cpu for query 93;
 
+# 4.执行计划 - 模拟优化器执行SQL查询语句，并返回相关的信息，
+# 程序员根据返回的信息判断是否进行优化或优化是否成功
+# 语法：desc | explain SQL查询语句 ;
+
+
+# 创建数据库
+CREATE DATABASE if not exists explaintest DEFAULT character set = utf8 ;
+use explaintest ;
+
+# 用户信息表
+CREATE TABLE userinfo
+(
+    `id` int  ,
+    `username` varchar(50)  ,
+    `password` varchar(50) ,
+    `sex` char(2),
+    `age` tinyint
+);
+
+INSERT into userinfo (id,username,password,sex,age) VALUES (1001,'zs','111111','男',18) ;
+INSERT into userinfo (id,username,password,sex,age) VALUES (1002,'ls','222222','女',28) ;
+INSERT into userinfo (id,username,password,sex,age) VALUES (1003,'ww','333333','男',38) ;
+
+
+# 订单信息表
+CREATE TABLE orderinfo
+(
+    `id` int  ,
+    `total` DECIMAL(10,1)  ,
+    `date` datetime,
+    `userid` int
+);
+
+INSERT into orderinfo(id,total,date,userid) values (11,55.4,'2022-02-21 11:11:11.111',1001) ;
+INSERT into orderinfo(id,total,date,userid) values (12,100,'2022-02-22 22:22:22.222',1002) ;
+INSERT into orderinfo(id,total,date,userid) values (13,136.6,'2022-02-23 12:12:12.122',1003) ;
+
+
+# 地址信息表
+create TABLE address
+(
+    `id` int  ,
+    `detail` varchar(100)  ,
+    `isdefault` bool,
+    `userid` int
+);
+
+insert into address(id,detail,isdefault,userid) values (21,'广东珠海',1,1001) ;
+insert into address(id,detail,isdefault,userid) values (22,'广东广州',1,1002) ;
+insert into address(id,detail,isdefault,userid) values (23,'广东中山',1,1003) ;
+insert into address(id,detail,isdefault,userid) values (24,'广东深圳',0,1003) ;
+
+
+
+
+explain select * from userinfo;
+
+/*
+    执行计划返回的信息有以下内容：
+
+    ● id：编号，表的读取和加载顺序
+    ● select_type：查询类型
+    ● table：表
+    ● type：类型
+    ● possible_keys：预测可能用到的索引，一个或多个
+    ● key：实际使用的索引，如果为NULL，则表示没有使用索引
+    ● key_len：实际使用索引的长度
+    ● ref：表之间的引用
+    ● rows：通过索引查询到的数据量
+    ● filtered：表示返回结果的行数占需读取行数的百分比，filtered的值越大越好
+    ● Extra：额外的信息
+ */
+
+##################
+# id字段
+# 案例一：id值相同，数据表按顺序查询
+select * from userinfo;
+select * from orderinfo;
+select * from address;
+
+# id值相同：从上往下的顺序执行
+explain
+    select * from userinfo t1,orderinfo t2 ,address t3
+        where t1.id=t2.userid
+          and t1.id = t3.userid;
+
+
+# 案例二（特例）：表的执行顺序会因表数量的改变而改变
+# 在userinfo表，添加三条新的数据，再执行以上同样的测试
+insert into userinfo (id,username,password,sex,age) values (1004,'zl','444444','女',31) ;
+insert into userinfo (id,username,password,sex,age) values (1005,'tq','555555','女',21) ;
+insert into userinfo (id,username,password,sex,age) values (1006,'sb','666666','男',22) ;
+
+
+explain select * from userinfo t1,orderinfo t2 ,address t3
+        where t1.id=t2.userid and t1.id = t3.userid;
+
+
+# id值不同:子查询，从大到小的顺序执行
+# 案例三：id值不同（嵌套子查询）
+explain
+select * from orderinfo t1
+where t1.userid =
+(
+  select t2.id from userinfo t2 where t2.id=
+  (
+      select t3.userid from address t3 where id=23
+  )
+);
+
+
+# 案例四：id值同时存在相同和不相同
+explain
+select * from orderinfo t1 ,userinfo t2
+where t1.userid = t2.id and t1.userid=
+(
+    select t3.userid from address t3 where id=23
+);
+
+
+##################
+# type字段
+# null > system > const > eq_ref> ref > range > index > ALL
+explain select '你好';
+
+explain
+select * from employees where emp_no='10001';
+
+show index from employees ;
+
+
+##################
+# Extra字段 - 额外信息
+# Using filesort:文件内排序
+# Using temporary：使用了临时表保存中间结果，临时表対系统性能损耗很大。
+# Using index：使用了覆盖索引
+
+# 覆盖索引：查询的字段覆盖了索引字段，也就是查询的都是索引字段。
+# select 字段1,字段2,... from 表;
+
+# 聚焦索引（主键索引）
+# 编号为主键，默认创建主键索引
+# 各节点存储有序的编号，在叶子节点中存储对应的整行数据
+# 因此，根据编号（主键）查询数据比较快！
+select * from 用户信息 where 编号=18 ;
+
+
+# 在表中，以帐号这个字段创建索引，则帐号就是索引字段
+# 以下查询，引用了 覆盖索引 查询，效率也是比较高的
+select 编号,帐号 from 用户信息 where 帐号='赵六' ;
+
+# 以下查询，并没有引用 覆盖索引 查询，效率比较低
+select 编号,帐号,性别 from 用户信息 where 帐号='赵六' ;
+select * from 用户信息 where 帐号='赵六' ;
+
+# 因此，在开发中，select 后面不要跟 *，而且建议使用 覆盖索引 查询
+
+
+
+
+
 
