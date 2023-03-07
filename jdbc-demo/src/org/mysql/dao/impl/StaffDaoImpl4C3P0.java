42abbdc515d12a25d532f66f48d01e9dc47c5ce0
zqx <114729040@qq.com>
2023-03-07 09:00:09 +0800

    DbUtils工具的使用

--- jdbc-demo/src/org/mysql/dao/impl/StaffDaoImpl4C3P0.java
+++ jdbc-demo/src/org/mysql/dao/impl/StaffDaoImpl4C3P0.java
@@ -1,6 +1,7 @@
 package org.mysql.dao.impl;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.mysql.dao.StaffDao;
 import org.mysql.entity.Staff;
@@ -25,8 +26,9 @@ public class StaffDaoImpl4C3P0 implements StaffDao {
         // 第一：定义要操作数据库的SQL语句
         String sql = "insert into staffs(name,age,phone,pos) values (?,?,?,?)" ;
         try {
+            Object[] params = new Object[]{ staff.getName(),staff.getAge(),staff.getPhone(),staff.getPos()} ;
             // 第二：填充数据，并执行
-            return qr.update(sql,staff.getName(),staff.getAge(),staff.getPhone(),staff.getPos()) ;
+            return qr.update(sql,params) ;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -34,12 +36,27 @@ public class StaffDaoImpl4C3P0 implements StaffDao {
 
     @Override
     public int delete(int id) {
-        return 0;
+        // 第一：定义要操作数据库的SQL语句
+        String sql = "delete from staffs where id=?" ;
+        try {
+            // 第二：填充数据，并执行
+            return qr.update(sql,id) ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int update(Staff staff) {
-        return 0;
+        // 第一：定义要操作数据库的SQL语句
+        String sql = "update staffs set name=?,age=?,phone=?,pos=? where id=?" ;
+        try {
+            Object[] params = new Object[]{ staff.getName(),staff.getAge(),staff.getPhone(),staff.getPos(),staff.getId()} ;
+            // 第二：填充数据，并执行
+            return qr.update(sql,params) ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -49,6 +66,7 @@ public class StaffDaoImpl4C3P0 implements StaffDao {
         String sql = "select id,name,age,phone,pos,add_time as addTime from staffs" ;
 
         // 第二：填充数据，并执行
+        // 查询是复杂 - 返回的结果是复杂多样的 - ResultSetHandler对象(接口) - 对查询的结果进行处理 - 根据查询的不同结果，定义相关实现类
         try {
             return qr.query(sql,new BeanListHandler<Staff>(Staff.class));
         } catch (SQLException e) {
@@ -58,6 +76,18 @@ public class StaffDaoImpl4C3P0 implements StaffDao {
 
     @Override
     public Staff selectById(int id) {
-        return null;
+        // 第一：定义要操作数据库的SQL语句
+        // 注意：使用别名解决实体对象属性名和数据表字段名不一致的问题
+        String sql = "select id,name,age,phone,pos,add_time as addTime from staffs where id=?" ;
+
+        // 第二：填充数据，并执行
+        // 查询是复杂 - 返回的结果是复杂多样的 - ResultSetHandler对象(接口) - 对查询的结果进行处理 - 根据查询的不同结果，定义相关实现类
+        try {
+            // List<Staff> list = qr.query(sql,new BeanListHandler<Staff>(Staff.class));
+            // return list!=null && list.size()==1?list.get(0):null ;
+            return qr.query(sql,new BeanHandler<Staff>(Staff.class)) ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
